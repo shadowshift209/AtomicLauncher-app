b752a9bb7b38ce786861c13732fcea4d2d89c234
Yosi Levy <atomictissue624@gmail.com>
2015-04-17 06:20:41 -0500

    Open Source!!!!

---
+++ src/net/yosi/isageek/atomiclauncher/LauncherAppActivity.java
@@ -0,0 +1,9 @@
+package net.yosi.isageek.atomiclauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import net.yosi.isageek.atomiclauncher.ui.LauncherActivity;
+
+public class LauncherAppActivity extends LauncherActivity {
+}
